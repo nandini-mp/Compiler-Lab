bdf488f7db3b1b3b55b96ac8a1cc9ff60fe0f520
Nandini <nandinimprakash.06@gmail.com>
2026-01-25 20:12:23 +0000

    Stage 3 Exercise 1 completed

---
+++ Stage3/Exercise1/control.h
@@ -0,0 +1,49 @@
+#ifndef CONTROL_H
+#define CONTROL_H
+
+typedef struct tnode{
+        int val;
+	int type;
+	char *varname;
+	int nodetype;
+	struct tnode *left,*right;
+} tnode;
+
+enum nodeTypes {
+	Nadd, Nsub, Nmul, Ndiv, Nassign, Nconnect, Nvar, Nconst, Nread, Nwrite, Nif, Nifelse, Ngt, Nlt, Nge, Nle, Nne, Neq, Nwhile
+};
+
+int getVariableIndex(tnode* root);
+
+int evaluate(tnode* root);
+
+void evaluateAndPrint(tnode* root);
+
+struct tnode* makeOperatorNode(char c,struct tnode *l,struct tnode *r);
+
+struct tnode* makeCOperatorNode(char c,struct tnode *l, struct tnode *r);
+
+struct tnode* makeConnectNode(tnode *l,tnode *r);
+
+struct tnode* makeVariableNode(char *c);
+
+struct tnode* makeConstantNode(int n);
+
+struct tnode* makeAssignNode(tnode *l,tnode* r);
+
+struct tnode* makeReadNode(tnode* t);
+
+struct tnode* makeWriteNode(tnode* t);
+
+void print(tnode* root);
+
+struct tnode* createTree(int val, int type, char* varname, int nodetype, struct tnode *l, struct tnode *r);
+
+struct tnode* makeIfElseNode(tnode* con, tnode* left, tnode* right);
+
+struct tnode* makeIfNode(tnode* con, tnode* stmt);
+
+struct tnode* makeWhileNode(tnode* con, tnode* stmt);
+
+#endif
+
